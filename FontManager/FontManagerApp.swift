23664596089af0f1d51c62864571c27cdb772ac1
alvin leung <alvinleung2009@gmail.com>
2025-05-22 18:56:31 -0700

    Initial Commit

---
+++ FontManager/FontManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontManagerApp.swift
+//  FontManager
+//
+//  Created by alvin leung on 2025-05-22.
+//
+
+import SwiftUI
+
+@main
+struct FontManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
